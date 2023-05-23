7fdb8300be519e215e20d9361f0504f236258eba
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-23 13:29:27 +0600

    show popular menu item add fiter and map

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 
 const PopularMenu = () => {
+    const [menu,setMenu] =useState([]);
+    fetch('menu.json')
+    .then(res => res.json())
+    .then(data => {
+        const populerItems = data.filter(item => item.category === 'popular');
+        setMenu(populerItems);
+    })
+
     return (
-        <section>
+        <section className="mb-12">
             <SectionTitle
-            heading={'From Our Menu'}
-            subHeading={'Popular Items'}
+                heading={'From Our Menu'}
+                subHeading={'Popular Items'}
             ></SectionTitle>
+            <div className="grid md:grid-cols-2 gap-10">
+            {
+                menu.map(item => <MenuItem 
+                    key={item._id}
+                    item={item}
+                >
+                </MenuItem>)
+            }
+            </div>
         </section>
+        
     );
 };
 
