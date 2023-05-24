79f6fd24ea5461cd1764be71bab5a28fe9f909d3
sawonhossain34 <sawonhossain3491@gmail.com>
2023-05-24 21:14:49 +0600

    menu Category this item show dynamic componemt

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,37 @@
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from '../../../assets/menu/pizza-bg.jpg';
-
+import dessertsImg from '../../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import soupImg from '../../../assets/menu/soup-bg.jpg';
+import saladImg from '../../../assets/menu/salad-bg.jpg';
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle"
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const soup = menu.filter(item => item.category === 'soup')
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const salad = menu.filter(item => item.category === 'salad')
+    const offered = menu.filter(item => item.category === 'offered')
+
     return (
         <div>
             <Cover img={menuImg} title="our menu"></Cover>
+            {/* main cover */}
+            <SectionTitle subHeading='Do not Miss' heading="today's offer"></SectionTitle>
+            {/* offer menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dezzad menu items */}
+            <MenuCategory 
+            items = {desserts}
+            title={'Desserts'}
+            img ={dessertsImg}
+            ></MenuCategory>
+            <MenuCategory items = {pizza} title={'Pizza'} img ={pizzaImg}></MenuCategory>
+            <MenuCategory items = {salad} title={'Salad'} img ={saladImg}></MenuCategory>
+            <MenuCategory items = {soup} title={'Soup'} img ={soupImg}></MenuCategory>
         </div>
     );
 };
