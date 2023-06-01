8eb4024a03a0a46c7282fe7a815e54a8e497ed2e
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-01 11:41:27 +0600

    imgbb fetch server load

--- src/pages/DashBoard/AddItem/AddItem.jsx
+++ src/pages/DashBoard/AddItem/AddItem.jsx
@@ -1,51 +1,68 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useForm } from 'react-hook-form';
 
+const image_hosting_token = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
 const AddItem = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data =>{
-         console.log(data)};
+    const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${image_hosting_token}`;
+    const onSubmit = data => {
+        const formData = new FormData();
+        formData.append('image',data.image[0])
+
+        fetch(img_hosting_url,{
+            method:'POST',
+            body:formData
+        })
+        .then(res => res.json())
+        .then(imgRespons => {
+            console.log(imgRespons);
+        })
+        
+    };
     console.log(errors);
+    console.log(image_hosting_token);
     return (
         <div className="w-full px-10">
             <SectionTitle subHeading="What's new" heading="Add an item"> </SectionTitle>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-control w-full max-w-xs">
+                <div className="form-control w-full my-4 ">
                     <label className="label">
                         <span className="label-text font-semibold">Recipe Name*</span>
                     </label>
-                    <input type="text" placeholder="Recipe Name"  {...register("name", {required: true, maxLength: 120})}  className="input input-bordered w-full max-w-xs" />
+                    <input type="text" placeholder="Recipe Name"  {...register("name", { required: true, maxLength: 120 })} className="input input-bordered w-full " />
                 </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Category*</span>
-                    </label>
-                    <select {...register("category", { required: true })} className="select select-bordered">
-                        <option disabled selected>Pick one</option>
-                        <option>Pizza</option>
-                        <option>Salad</option>
-                        <option>Drinks</option>
-                        <option>Soup</option>
-                        <option>Dessert</option>
-                    </select>
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text font-semibold">Price*</span>
-                    </label>
-                    <input type="number" placeholder="Type here"  {...register("price", {required: true, maxLength: 120})} className="input input-bordered w-full max-w-xs" />
+                <div className="flex  my-4">
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text">Category*</span>
+                        </label>
+                        <select defaultValue="Pick One"{...register("category", { required: true })} className="select select-bordered">
+                            <option disabled>Pick One</option>
+                            <option>Pizza</option>
+                            <option>Salad</option>
+                            <option>Drinks</option>
+                            <option>Soup</option>
+                            <option>Dessert</option>
+                        </select>
+                    </div>
+                    <div className="form-control w-full ml-4 ">
+                        <label className="label">
+                            <span className="label-text font-semibold">Price*</span>
+                        </label>
+                        <input type="number" placeholder="Type here"  {...register("price", { required: true, maxLength: 120 })} className="input input-bordered w-full " />
+                    </div>
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Recipe Details*</span>
                     </label>
-                    <textarea className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                    <textarea  {...register("recipe", { required: true, maxLength: 120 })} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
                 </div>
-                <div className="form-control w-full max-w-xs">
+                <div className="form-control w-full my-4 ">
                     <label className="label">
                         <span className="label-text">Item Image</span>
                     </label>
-                    <input type="file"  {...register("details", {required: true, maxLength: 120})} className="file-input file-input-bordered w-full max-w-xs" />
+                    <input type="file"  {...register("image", { required: true, maxLength: 120 })} className="file-input file-input-bordered w-full " />
                 </div>
                 <input className="btn btn-sm mt-4" type="submit" value="Add Item" />
             </form>
