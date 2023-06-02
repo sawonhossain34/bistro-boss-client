9db739b4ba456d40933da67ca0ab34495cb4cee3
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-02 10:26:03 +0600

    stripe account create and public key set loadstripe

--- src/pages/DashBoard/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment.jsx
@@ -1,11 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
 
 const Payment = () => {
+    // TODO : provide published key
+    const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
     return (
         <div>
             <SectionTitle subHeading='Please Process' heading="Payment"></SectionTitle>
             <h3 className="text-3xl">o taka o taka tumi uira uira aco</h3>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
         </div>
     );
 };
