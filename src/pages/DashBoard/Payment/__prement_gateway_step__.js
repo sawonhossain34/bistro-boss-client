ef9fbf93533ecb6061cfc5c77280e7b5bc5b43e7
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-02 11:36:21 +0600

    set payment Method and text stripe and error handleing

---
+++ src/pages/DashBoard/Payment/__prement_gateway_step__.js
@@ -0,0 +1,6 @@
+/**
+ * 1 . install stripe and react stripe.js
+ * 2 . create a cheackOut form with card element(card element contains:card number,expiration date,cvs,zip code)
+ * 3 . Create account on stripe and get the publishable key pk.
+ * 4 . 
+ */
