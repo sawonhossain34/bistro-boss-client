4c6ffd75aedb76a0c9b892ce8f8b322d6142365d
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-02 16:23:31 +0600

    price reduce and parseFloat tofixed

--- src/pages/DashBoard/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment.jsx
@@ -2,17 +2,21 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
+import useCart from "../../../hooks/useCart";
 
 
 const Payment = () => {
     // TODO : provide published key
     const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+    const [cart] = useCart();
+    const total = cart.reduce( (sum,item) => sum + item.price , 0);
+    const price = parseFloat(total.toFixed(2))
     return (
         <div>
             <SectionTitle subHeading='Please Process' heading="Payment"></SectionTitle>
             <h3 className="text-3xl">o taka o taka tumi uira uira aco</h3>
             <Elements stripe={stripePromise}>
-                <CheckoutForm></CheckoutForm>
+                <CheckoutForm price={price}></CheckoutForm>
             </Elements>
         </div>
     );
