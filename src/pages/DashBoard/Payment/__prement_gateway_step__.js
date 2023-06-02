e44266e8a708fff5ae8bfc10b64d6c6f20af1e30
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-02 15:53:07 +0600

    payment gateway key add

--- src/pages/DashBoard/Payment/__prement_gateway_step__.js
+++ src/pages/DashBoard/Payment/__prement_gateway_step__.js
@@ -2,5 +2,7 @@
  * 1 . install stripe and react stripe.js
  * 2 . create a cheackOut form with card element(card element contains:card number,expiration date,cvs,zip code)
  * 3 . Create account on stripe and get the publishable key pk.
- * 4 . 
+ * 4 . get card information
+ * 5 . create a payment method
+ * 6 . use test card and to test payment 
  */
