3bc599d2b56f12f2293922178e65e835ab8fe4a3
sawonhossain34 <sawonhossain3491@gmail.com>
2023-06-02 07:43:34 +0600

    delete button add manageItem and delete database

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -11,14 +11,14 @@ const useMenu = () => {
     //             setLoading(false);
     //         })
     // }, [])
-    const {data:menu =[], isLoading:loading} = useQuery({
+    const {data:menu =[], isLoading:loading,refetch} = useQuery({
         queryKey:['menu'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/menu');
             return res.json();
         }
     })
-    return [menu,loading];
+    return [menu,loading,refetch];
 }
 
 export default useMenu;
--- src/pages/DashBoard/ManageItems/ManageItems.jsx
+++ src/pages/DashBoard/ManageItems/ManageItems.jsx
@@ -1,9 +1,41 @@
+import { FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
 const ManageItems = () => {
-    const [menu] = useMenu();
+    const [menu, , refetch] = useMenu();
+    const [axiosSecure] = useAxiosSecure();
+
+    const handleDelete = item => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/menu/${item._id}`)
+                    .then(res => {
+                        console.log('deleted res', res.data);
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
     return (
         <div className="w-full">
             <SectionTitle subHeading="Manage All Item" heading="Hurry Up"></SectionTitle>
@@ -12,51 +44,45 @@ const ManageItems = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>#</th>
+                            <th>Item</th>
+                            <th>Category</th>
+                            <th>Price</th>
                             <th>Update</th>
                             <th>Delete</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            menu.map(item => <tr key={item._id}>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div className="flex items-center space-x-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                            menu.map((item, index) => <tr key={item._id}>
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{item.name}</div>
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Zemlak, Daniel and Leannon
-                            </td>
-                            <td>Purple</td>
-                            <td>
-                                <button className="btn btn-ghost btn-xs">Update</button>
-                            </td>
-                            <td>
-                                <button className="btn btn-ghost btn-xs">Delete</button>
-                            </td>
-                        </tr>)
+                                </td>
+                                <td>
+                                    {item.category}
+                                </td>
+                                <td className="text-right">${item.price}</td>
+                                <td>
+                                    <button className="btn btn-ghost btn-xs">Update</button>
+                                </td>
+                                <td>
+                                    <button onClick={() => handleDelete(item)} className="btn bg-red-600 text-white btn-ghost "><FaTrashAlt></FaTrashAlt></button>
+                                </td>
+                            </tr>)
                         }
-                         
+
                     </tbody>
                 </table>
             </div>
